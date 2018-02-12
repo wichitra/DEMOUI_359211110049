dcd175ffa096c0b26c3b11c928ce1f85794b5b3b
wichitra <z3rfon@gmail.com>
2018-02-12 15:06:40 +0700

    UI Design and Create DB

---
+++ src/dbUtil/dbConnection.java
@@ -0,0 +1,23 @@
+package dbUtil;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class dbConnection {
+    private static final String SQCONN ="jdbc:sqlite:school.sqlite";
+    public static Connection getConnection(){
+        try {
+            Class.forName("org.sqlite.JDBC");
+            return DriverManager.getConnection(SQCONN);
+
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (SQLException e) {
+
+        }
+        return null;
+    }//getConnection
+    
+}//class
+
